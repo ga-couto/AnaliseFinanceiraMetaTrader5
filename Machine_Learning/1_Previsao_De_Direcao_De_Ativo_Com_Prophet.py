a9a11fe0ace54a007b278341a93f10bf7383a644
ga-couto <gabrielcoutocabral.gc@gmail.com>
2022-10-20 23:32:21 -0300

    Implementado um machine learning utilizando Prophet para prever a direção de um ativo.

--- Machine_Learning/1_Previsao_De_Direcao_De_Ativo_Com_Prophet.py
+++ Machine_Learning/1_Previsao_De_Direcao_De_Ativo_Com_Prophet.py
@@ -6,13 +6,14 @@ import matplotlib.pyplot as plt
 from prophet import Prophet
 
 #Importando dados do "Ativo"
-data = yf.download('MGLU3.SA',start='2018-01-01', end=datetime.datetime.today())
+data = yf.download('PETR4.SA',start='2018-01-01', end=datetime.datetime.today())
 print(data)
 
 #Criando dataframe vazio
-df = pd.Dataframe()
-df['y'] = data['close']
+df = pd.DataFrame()
+df['y'] = data['Close']
 df['ds'] = data.index
+df['ds'] = df['ds'].dt.tz_localize(None)
 
 print(df)
 
@@ -22,4 +23,4 @@ modelo.fit(df)
 futuro = modelo.make_future_dataframe(periods=35)
 predictions = modelo.predict(futuro)
 modelo.plot(predictions)
-print(predictions.tail)
+print(predictions.tail(n=50))
