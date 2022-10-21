10552df674eef08aa7c518cb6ed3128bfc729b0b
ga-couto <gabrielcoutocabral.gc@gmail.com>
2022-10-20 23:37:09 -0300

    Iniciado modelo de regressão com Machine Learning.

---
+++ Machine_Learning/2_MachineLearning_Regressao.py
@@ -0,0 +1,8 @@
+import MetaTrader5 as mt5
+from datetime import datetime
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import talib
+import seaborn as sns
+import statsmodels.api as sm
